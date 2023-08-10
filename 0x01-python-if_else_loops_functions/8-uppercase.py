a069c3274a955d49396ba96adda04a83c9d8e7a7
gnxrlyqf <gnarlydestructor@gmail.com>
2023-08-10 03:42:21 +0100

    01

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-def upper(str):
+def uppercase(str):
     for c in str:
         if (ord(c) >= 97 and ord(c) <= 124):
-            print("{}".format(chr(ord(c) - 32)))
+            print("{}".format(chr(ord(c) - 32)), end="")
         else:
-            print(c)
+            print(c, end="")
+            
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print("{}".format(number % 10 if number >= 0 else (-number) % 10))
+    print(.format(number % 10 if number >= 0 else (-number) % 10))
+    return (number % 10 if number >= 0 else (-number) % 10)
