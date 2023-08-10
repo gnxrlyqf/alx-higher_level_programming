12733844b59be1093ad9fa36f463af8fb8ed22eb
gnxrlyqf <gnarlydestructor@gmail.com>
2023-08-10 04:24:35 +0100

    00-10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -3,29 +3,6 @@
 #include <stdio.h>
 
 /**
- * exist_in_list - checks if an element exists in a linked list
- * @pointer: pointer to the next element in the list
- * @arr: array to check
- * @size: size of the array
- *
- * Return: 1 (exists), 0 (doesn't exist)
-*/
-
-int	exist_in_list(listint_t *pointer, int size, listint_t **arr)
-{
-	int	i;
-
-	i = 0;
-	while (i < size)
-	{
-		if (pointer == arr[i])
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-/**
  * check_cycle - checks a linked list for a cycle
  * @list: head node of the linked list to check
  *
@@ -34,20 +11,13 @@ int	exist_in_list(listint_t *pointer, int size, listint_t **arr)
 
 int check_cycle(listint_t *list)
 {
-	listint_t	**arr;
-	int			size;
 	listint_t	*list_item;
 
-	size = 2;
 	list_item = list;
-	arr = (listint_t **)malloc(2 * sizeof(listint_t *));
-	arr[0] = list;
 	while (list_item->next)
 	{
-		if (exist_in_list(list_item->next, size, arr))
+		if (list_item->next == list)
 			return (1);
-		arr = (listint_t **)realloc(arr,(++size) * sizeof(listint_t *));
-		arr[size - 1] = list_item->next;
 		list_item = list_item->next;
 	}
 	return (0);
