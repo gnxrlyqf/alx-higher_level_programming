115edd28fd947f19b2ca2c590b9536a7fb203e29
gnxrlyqf <gnarlydestructor@gmail.com>
2023-08-11 01:36:52 +0100

    bro

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for c in ["zYxWvUtSrQpOnMlKjIhGfEdCbA".strip()]:
+    print(c)
---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    string = ""
+    for i in range (len(str)):
+        string += str[i] if i != n else ""
+    return string
