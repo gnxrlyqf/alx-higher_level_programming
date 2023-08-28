b2f3f6ddb110fa27a72ea33bdbcdcaa39ef14ee8
gnxrlyqf <gnarlydestructor@gmail.com>
2023-08-29 00:51:16 +0100

    4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,7 +2,7 @@
 def list_division(my_list_1, my_list_2, list_length):
     new_list = [0] * list_length
     result = 0
-    for i in range (list_length):
+    for i in range(list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
         except IndexError:
