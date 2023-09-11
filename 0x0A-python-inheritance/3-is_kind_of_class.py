f0696d19bfc8a8ae2a2c9a88bcb016326bd99c73
gnxrlyqf <gnarlydestructor@outlook.com>
2023-09-11 18:08:17 +0100

    3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""checks a class"""
+
+
+def is_same_class(obj, a_class):
+    """check if object is exactly an instance of class
+
+    Args:
+        obj (any): object to check
+        a_class (type): class to check type of obj
+    Returns:
+        obj is exactly an instance of a_class - True
+        Otherwise - False
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
