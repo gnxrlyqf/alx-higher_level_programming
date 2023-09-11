b51238b88ce0d8c15de1e9f3e1f119f08671e16c
gnxrlyqf <gnarlydestructor@outlook.com>
2023-09-11 22:53:04 +0100

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defines base geometry class"""
+
+
+class BaseGeometry:
+    """base geometry"""
+
+    def area(self):
+        """no implementation"""
+        raise Exception("area() is not implemented")
