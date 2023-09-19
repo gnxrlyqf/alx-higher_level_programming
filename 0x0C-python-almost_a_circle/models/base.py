0c097c65afd01cbafe52a804b0b061485a7c1dc5
gnxrlyqf <gnarlydestructor@outlook.com>
2023-09-19 02:22:39 +0100

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 
+
 class Base:
-	__nb_objects = 0
-	def __init__(self, id=None):
-		if id is not None:
-			self.id = id
-		else:
-			Base.__nb_objects += 1
-			self.id = Base.__nb_objects
+    """base model for all models inheriting from it
+
+    Private Class Attributes:
+        __nb_object (int): number of objects instantiated
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
