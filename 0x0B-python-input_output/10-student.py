5a13838522db2ab6938783a03287955a85ce29a7
gnxrlyqf <gnarlydestructor@outlook.com>
2023-10-11 00:04:26 +0100

    10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -18,10 +18,12 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """represent student as dictionary"""
-        if (type(attrs) == list and 
-            all(type(elem) == str for elem in attrs)):
-            new_dictionary = {} 
+        """represent student as dictionary
+            Args:
+        attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and all(type(e) == str for e in attrs)):
+            new_dictionary = {}
             for k in attrs:
                 if hasattr(self, k):
                     new_dictionary[k] = getattr(self, k)
