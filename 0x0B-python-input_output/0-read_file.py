bca61276ccfa15bbe9a6b41b27cde359d510fff7
gnxrlyqf <gnarlydestructor@outlook.com>
2023-10-10 12:49:45 +0100

    1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """read and print a file"""
-    with open(filename, encoding = "utf-8") as file:
-        print(file.read(), end = "")
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""file writing function"""
+
+
+def read_file(filename="", text=""):
+    """read and print a file
+    Args:
+        filename (str): file to write to
+        text (str): text to write
+    Returns:
+        number of bytes written
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
