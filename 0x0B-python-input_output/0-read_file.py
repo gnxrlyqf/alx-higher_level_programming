5e45d73d4321734fbb008187ac3aa364a16ddb2f
gnxrlyqf <gnarlydestructor@outlook.com>
2023-10-10 12:01:35 +0100

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """read and print a file"""
     with open(filename, encoding = "utf-8") as file:
-        print(file.readlines())
+        print(file.read(), end = "")
