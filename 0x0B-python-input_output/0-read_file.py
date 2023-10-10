a9d265794cdd068498e5b7277a18caa108007c3d
gnxrlyqf <gnarlydestructor@outlook.com>
2023-10-10 11:58:13 +0100

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""file reading function"""
+
+
 def read_file(filename=""):
-	with open(filename) as f
-	print(f.readlines())
+    """read a file
+    Args:
+        filename (str): file name to read
+    """
+    with open(filename) as f
+    print(f.readlines())
