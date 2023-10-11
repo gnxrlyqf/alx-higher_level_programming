1c2d17b5b054abe7881596846575f16cb54adaed
gnxrlyqf <gnarlydestructor@outlook.com>
2023-10-12 00:51:31 +0100

    8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""rectangle class inheriting from basegeometry"""
+parent = __import__('7-base_geometry').parent
+
+
+class Rectangle(parent):
+    """represent rectangle"""
+
+    def __init__(self, width, height):
+        """rectangle constructor
+
+        Args:
+            width (int): rectangle width
+            height (int): rectangle height
+        """
+        self.integer_validator("width", width)
+        self.width = width
+        self.integer_validator("height", height)
+        self.height = height
