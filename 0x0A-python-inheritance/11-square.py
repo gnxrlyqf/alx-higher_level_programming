1efa0d9bca4ad075287659be5645703894ca41b7
gnxrlyqf <gnarlydestructor@outlook.com>
2023-10-12 02:10:03 +0100

    11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -15,3 +15,7 @@ class Square(parent):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+	def __str__(self):
+        """represent square as string"""
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
