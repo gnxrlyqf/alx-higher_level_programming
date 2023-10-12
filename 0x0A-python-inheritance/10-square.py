de8ccd52e51011538c7f23f147d3f50ec8737fbc
gnxrlyqf <gnarlydestructor@outlook.com>
2023-10-12 02:07:02 +0100

    10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""defines square class inheriting from rectangle"""
+parent = __import__('9-rectangle').Rectangle
+
+
+class Square(parent):
+    """represent square"""
+
+    def __init__(self, size):
+        """square constructor
+
+        Args:
+            size (int): square size
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
