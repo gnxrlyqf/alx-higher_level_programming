a65b52c9cec6246fd6964357031ac3e5ac03bfbc
gnxrlyqf <gnarlydestructor@outlook.com>
2023-10-12 02:09:14 +0100

    11

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -23,5 +23,5 @@ class Rectangle(parent):
         return self.__width * self.__height
 
     def __str__(self):
-        """Return the print() and str() representation of a Rectangle."""
+        """represent rectangle as string"""
         return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
