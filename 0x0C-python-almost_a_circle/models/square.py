cb166bf03d32a4b25b33f92572ab79e4af20ec12
gnxrlyqf <gnarlydestructor@outlook.com>
2023-10-16 22:31:16 +0100

    both

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -68,3 +68,12 @@ class Square(Rectangle):
                     self.x = value
                 elif key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """represent square as dictionary"""
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
