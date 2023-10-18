0a521cc2d16e1cfb27d6325168382b1c575aa001
gnxrlyqf <gnarlydestructor@outlook.com>
2023-10-18 11:06:52 +0100

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,7 +21,11 @@ class Base:
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
-        """return string representation of a list"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        """return string representation of a list
+
+        Args:
+            list_dictionaries (list): list to represent as json dictionary
+        """
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
