b21ab80c3759f486b22909c8da09b0c65df5722e
gnxrlyqf <gnarlydestructor@outlook.com>
2023-10-18 11:03:03 +0100

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """define base class"""
 import json
 
+
 class Base:
     """base class
 
@@ -21,4 +22,6 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """return string representation of a list"""
+        if list_dictionaries is None:
+            return "[]"
         return json.dumps(list_dictionaries)
