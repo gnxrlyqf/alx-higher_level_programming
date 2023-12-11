9af0ddc05662c0b0412a0d1a85318b648b31189b
gnxrlyqf <gnarlydestructor@outlook.com>
2023-12-11 19:11:01 +0100

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argv = process.argv.slice(2).map(Number);
 if (argv.length < 2) {
-  console.log(0);
+  console.log('0');
 } else {
   const arr = argv.sort();
   console.log(arr[arr.length - 2]);
