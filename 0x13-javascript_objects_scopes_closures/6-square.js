11a5aadc3b4bef33aa53f5f56a76eff9bc08086e
gnxrlyqf <gnarlydestructor@outlook.com>
2023-12-12 19:59:35 +0100

    6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,19 +2,12 @@
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c) {
-    if (c) {
+    if (c === undefined) {
+      c = 'X'
+    }
       for (let i = 0; i < this.height; i++) {
         console.log(c.repeat(this.width));
-      }
-    } else {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
     }
   }
 }
