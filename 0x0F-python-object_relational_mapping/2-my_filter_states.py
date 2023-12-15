4b11b79d969238cdb009cba2eae6357fbeb52d04
gnxrlyqf <gnarlydestructor@outlook.com>
2023-12-15 14:33:28 +0100

    2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Prints data that starts with N in a database"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3],
+        port=3306
+    )
+    cursor = db.cursor()
+    cursor._query("SELECT * FROM states WHERE name LIKE '" + sys.argv[4] + "%'")
+    data = cursor.fetchall()
+    for row in data:
+        print(row)
+    cursor.close()
+    db.close()
