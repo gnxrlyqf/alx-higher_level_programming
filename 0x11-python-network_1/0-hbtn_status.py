9587a785826a1c66259716555ea2d68ef408fc2d
gnxrlyqf <gnarlydestructor@outlook.com>
2024-01-06 16:40:21 +0100

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     with request.urlopen("https://alx-intranet.hbtn.io/status") as page:
         data = page.read()
         print("Body response:")
-        print("\t- type: ", type(data))
-        print("\t- content: ", data)
-        print("\t- utf8 content: ", data.decode("utf-8"))
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode("utf-8")))
