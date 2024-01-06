d96fc4a8dd3c7be818f8575580953b25f5de24b0
gnxrlyqf <gnarlydestructor@outlook.com>
2024-01-06 17:10:18 +0100

    2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""send a post request to a url"""
+from urllib import parse, request
+import sys
+if __name__ == "__main__":
+    email = {"email": sys.argv[2]}
+    string = parse.urlencode(email)
+    req = request.urlopen(sys.argv[1], data=string, method="POST")
+    with request.urlopen(request) as response:
+        out = response.read().decode("utf-8")
+        print(out)
