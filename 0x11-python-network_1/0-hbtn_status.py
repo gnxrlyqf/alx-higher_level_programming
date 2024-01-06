a42f1d1ceb83be0ead2b2189bd0517c28d40e95a
gnxrlyqf <gnarlydestructor@outlook.com>
2024-01-06 16:38:59 +0100

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,10 @@
 """fetches data from url"""
 
 from urllib import request
-with request.urlopen("https://alx-intranet.hbtn.io/status") as page:
-    data = page.read()
-    print("Body response:")
-    print("\t- type: ", type(data))
-    print("\t- content: ", data)
-    print("\t- utf8 content: ", data.decode("utf-8"))
+if __name__ == "__main__":
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as page:
+        data = page.read()
+        print("Body response:")
+        print("\t- type: ", type(data))
+        print("\t- content: ", data)
+        print("\t- utf8 content: ", data.decode("utf-8"))
