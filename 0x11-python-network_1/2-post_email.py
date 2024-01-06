ddca2631ab89362d3a61b406a99de7aa1a27fc7f
gnxrlyqf <gnarlydestructor@outlook.com>
2024-01-06 17:17:19 +0100

    2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     data = {"email": sys.argv[2]}
     data = parse.urlencode(data)
     data = data.encode("ascii")
-    req = request.urlopen(sys.argv[1], data=string)
+    req = request.urlopen(sys.argv[1], data=data)
     with request.urlopen(request) as response:
         out = response.read().decode("utf-8")
         print(out)
