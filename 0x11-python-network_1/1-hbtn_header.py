4affba540d068834d1a2fb30244eee98844b865b
gnxrlyqf <gnarlydestructor@outlook.com>
2024-01-06 16:54:07 +0100

    1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 """displays header value of a webpage"""
-
+from urllib import request
+import sys
+if __name__ == "__main__":
+    with request.urlopen(sys.argv[1]) as page:
+        header = page.getheader("X-Request-Id")
+        print(header)
