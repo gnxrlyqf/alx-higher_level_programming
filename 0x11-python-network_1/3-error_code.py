de9d2447f6e6a48ed844a49bd2a2ac54f95b1873
gnxrlyqf <gnarlydestructor@outlook.com>
2024-01-07 13:12:35 +0100

    3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-from urllib import request
+from urllib import request, error
 import sys
 if __name__ == "__main__":
-    with request.urlopen(sys.argv[1]) as page:
-        data = page.read()
-        print(data.decode("utf-8"))
+    try:
+        with request.urlopen(sys.argv[1]) as page:
+            data = page.read()
+            print(data.decode("utf-8"))
+    except error.HTTPError as err:
+        print("Error code: {}".format(err.code))
