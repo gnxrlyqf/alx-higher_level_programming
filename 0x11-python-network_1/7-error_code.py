3bcf8e1b69bbb3b35a0bdf73198ad66fae9c3b98
gnxrlyqf <gnarlydestructor@outlook.com>
2024-01-07 16:38:40 +0100

    7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -3,8 +3,8 @@
 from sys import argv
 from requests import get, HTTPError
 if __name__ == "__main__":
-        with get(argv[1]) as page:
-            if page.status_code >= 400:
-                print("Error code: {}".format(page.status_code))
-            else:
-                print(page.content.decode("utf-8"))
+    with get(argv[1]) as page:
+        if page.status_code >= 400:
+            print("Error code: {}".format(page.status_code))
+        else:
+            print(page.content.decode("utf-8"))
