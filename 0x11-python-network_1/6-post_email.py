abd45afdfc52d037e7be3ae0b3cfa34bac960364
gnxrlyqf <gnarlydestructor@outlook.com>
2024-01-07 16:21:33 +0100

    6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,4 +5,4 @@ from requests import post
 if __name__ == "__main__":
     data = {"email": argv[2]}
     with post(argv[1], data=data) as reponse:
-        print(reponse)
+        print(reponse.content.decode("utf-8"))
