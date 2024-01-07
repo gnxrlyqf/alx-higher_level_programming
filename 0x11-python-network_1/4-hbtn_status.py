7a5ac8076b01b07d578dcf93a5fad83ff7dc73f4
gnxrlyqf <gnarlydestructor@outlook.com>
2024-01-07 15:36:53 +0100

    4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import requests
+if __name__ == "__main__":
+    with requests.get("https://alx-intranet.hbtn.io/status") as page:
+        content = page.content.decode("utf-8")
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
